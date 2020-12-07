34fc4f5f7fa8b4dd6523cedf86b0da95eaa88abb
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2020-12-07 13:35:12 +0100

    master upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JensNeergaardBiblotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool killMe = false;
+            while (!killMe)
+            {
+
+            }
+        }
+    }
+}
